79f8e5b01e4234d2eaad9737ee0a53f8e2eb107f
Arushad Ahmed <dash-8x@hotmail.com>
2021-06-25 15:30:10 +0500

    - Tested on WP 5.7 version

---
+++ javaabu-thaana/res/js/jtk-admin.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+	$('input#custom-menu-item-name, input.edit-menu-item-title, input#blogname, input#blogdescription, input#first_name, input#last_name, input#title, input#new-tag-post_tag, input#newcategory, input#tag-name, textarea#tag-description, textarea#attachment_caption, textarea#attachment_content, input.edit-menu-item-title')
+		.each(function () {
+			$(this).thaana().addClass('dv-input');
+		});
+});
